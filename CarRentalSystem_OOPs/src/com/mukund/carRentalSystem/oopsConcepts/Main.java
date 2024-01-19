b0342f307d06ba2a331ce225feb9f5198d76b8af
Mukund Lokhande <mukundlokhande926@gmail.com>
2024-01-20 01:45:09 +0530

    CarRentalSystem: OOPs Based Project

---
+++ CarRentalSystem_OOPs/src/com/mukund/carRentalSystem/oopsConcepts/Main.java
@@ -0,0 +1,19 @@
+package com.mukund.carRentalSystem.oopsConcepts;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CarRentalSystem rentalSystem = new CarRentalSystem();
+
+        Car car1 = new Car("C001", "Toyota", "Camry", 60.0); // Different base price per day for each car
+        Car car2 = new Car("C002", "Honda", "Accord", 70.0);
+        Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
+        rentalSystem.addCar(car1);
+        rentalSystem.addCar(car2);
+        rentalSystem.addCar(car3);
+
+        rentalSystem.menu();
+	}
+
+}
---
+++ CarRentalSystem_OOPs/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CarRentalSystem_OOPs {
+}
